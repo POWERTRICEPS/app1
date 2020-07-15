8f323fdb07a9d0c9c453e6ed5ba84c5af2d54cdc
POWERTRICEPS <imnotdacoco05@gmail.com>
2020-07-15 16:09:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,22 @@ from datetime import datetime
 from pathlib import Path
 import random
 
-
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
-    def login(self):
-        self.manager.current = "welcome_screen"
-
+    
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = "welcome_screen"
+            print("Hello")
+        else:
+            print("afafa")
+        
+            
 class RootWidget(ScreenManager):
     pass
 
@@ -31,13 +38,19 @@ class SignUpScreen(Screen):
         with open("users.json", 'w') as file:
             json.dump(users, file)
         print("yahoo!")
+        self.manager.current = "sign_up_screen_success"
 
     def go_back(self):
         self.manager.current = "login_screen"
 
+class SignUpScreenSuccess(Screen):
+    pass
+
 
 class HomeScreen(Screen):
-    pass
+    def log_out(self):
+        self.manager.transition.direction = "up"
+        self.manager.current = "login_screen"
 
 class WelcomeScreen(Screen):
     pass
@@ -45,6 +58,7 @@ class WelcomeScreen(Screen):
 class MainApp(App):
     def build(self):
         return RootWidget()
+    
 
 if __name__ == "__main__":
     MainApp().run()
